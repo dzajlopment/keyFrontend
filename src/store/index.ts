15caec29bae0bc4e5b687cdab8b349a449dbecff
LordPrinz <plazinski2902@gmail.com>
2023-10-07 20:27:56 +0200

    Add ws slice to reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import teacherSlice from "./slices/teacherSlice";
 import keySlice from "./slices/keySlice";
 import { Key, Teacher } from "../types/models";
+import websocketSlice from "./slices/websocketSlice";
 
 export type ReduxState = {
 	teachers: Teacher[];
 	keys: Key[];
+	ws: WebSocket;
 };
 
 const store = configureStore({
 	reducer: {
 		teachers: teacherSlice,
 		keys: keySlice,
+		ws: websocketSlice,
 	},
 });
 
