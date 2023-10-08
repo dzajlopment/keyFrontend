03d0a3ae19d0fb24a21fd719d2fc79e7fefe1c23
LordPrinz <plazinski2902@gmail.com>
2023-10-08 16:00:13 +0200

    Implement custom modal

--- src/components/KeyBox/Key.tsx
+++ src/components/KeyBox/Key.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { type Key as KeyModel } from "../../types/models";
 import { cn } from "../../utils/tailwind";
+import Modal from "../UI/Modal";
 
 export interface KeyProps
 	extends React.HTMLAttributes<HTMLButtonElement>,
@@ -7,12 +9,25 @@ export interface KeyProps
 
 export const Key = ({ name, owner, className, ...props }: KeyProps) => {
 	const isEnabled = !!owner; // Detect if there is a owner
+	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<button
-			className={cn("key", isEnabled ? "key-enabled" : "key-disabled", className)} //TODO: Style button and its variations.
-			{...props}>
-			{name}
-		</button>
+		<>
+			{isOpen && (
+				<Modal
+					title="Filtr"
+					children={<div>XD</div>}
+					onClick={() => {
+						setIsOpen(false);
+					}}
+				/>
+			)}
+			<button
+				onClick={() => setIsOpen(true)}
+				className={cn("key", isEnabled ? "key-enabled" : "key-disabled", className)} //TODO: Style button and its variations.
+				{...props}>
+				{name}
+			</button>
+		</>
 	);
 };
