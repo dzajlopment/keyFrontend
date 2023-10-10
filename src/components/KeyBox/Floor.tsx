307cc141b2c4044131f18091846cc86d53e9233d
LordPrinz <plazinski2902@gmail.com>
2023-10-10 18:58:34 +0200

    Update styling

--- src/components/KeyBox/Floor.tsx
+++ src/components/KeyBox/Floor.tsx
@@ -8,12 +8,12 @@ export type Props = {
 export const Floor = ({ name, keys }: Props) => {
 	return (
 		<div className="rounded-xl p-5">
-			<div className="text-3xl font-medium text-slate-800 tracking-tight">
+			<div className="text-3xl font-medium text-slate-800 tracking-tight ">
 				{name}
 			</div>
 			<div className="grid mt-5 gap-5 auto-fit-grid ">
 				{keys.map((key) => (
-					<Key key={key.name} {...key} className="aspect-[3/2]" />
+					<Key key={key.name} {...key} className="aspect-[4/3]" />
 				))}
 			</div>
 		</div>
--- src/components/KeyBox/index.tsx
+++ src/components/KeyBox/index.tsx
@@ -9,14 +9,14 @@ const KeyBox = () => {
 	const { keys } = useKeys();
 
 	return (
-		<section className="relative bg-slate-50 rounded-xl overflow-hidden h-full">
+		<section className="relative bg-slate-50  h-full select-none">
 			<div
 				style={{
 					backgroundPosition: "10px 10px",
 				}}
-				className="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
+				className="absolute h-full inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
 			<div className="relative rounded-xl overflow-auto">
-				<div className="flex flex-col">
+				<div className="flex flex-col gap-3 m-6">
 					<Floor name="Piwnica / Parter" keys={getFloorKeys(keys, 0)} />
 					<Floor name="Pierwsze piętro" keys={getFloorKeys(keys, 1)} />
 					<Floor name="Drugie piętro" keys={getFloorKeys(keys, 2)} />
