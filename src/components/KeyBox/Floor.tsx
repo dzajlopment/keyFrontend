5ebafb56757bf9e9fdf28657a0c5640a34656a82
LordPrinz <plazinski2902@gmail.com>
2023-10-10 17:38:47 +0200

    Prepare to light mode

--- src/components/KeyBox/Floor.tsx
+++ src/components/KeyBox/Floor.tsx
@@ -5,15 +5,13 @@ export type Props = {
 	keys: KeyProps[];
 };
 
-// TODO: Style this component
-
 export const Floor = ({ name, keys }: Props) => {
 	return (
-		<div className="bg-purple-600 rounded-xl p-5 border-2 border-white">
-			<div className="text-center text-2xl text-white font-bold italic mb-5">
+		<div className="rounded-xl p-5">
+			<div className="text-2xl font-medium text-slate-900 tracking-tight">
 				{name}
 			</div>
-			<div className="grid gap-3 sm:gap-5 lg:gap-8 xl:gap-12 grid-cols-4 sm:grid-cols-6 lg:grid-cols-8">
+			<div className="grid mt-5 ">
 				{keys.map((key) => (
 					<Key key={key.name} {...key} />
 				))}
--- src/components/KeyBox/index.tsx
+++ src/components/KeyBox/index.tsx
@@ -9,12 +9,19 @@ const KeyBox = () => {
 	const { keys } = useKeys();
 
 	return (
-		<section className="bg-black py-10">
-			<div className="container flex flex-col gap-8">
-				<Floor name="Piwnica / Parter" keys={getFloorKeys(keys, 0)} />
-				<Floor name="Pierwsze piętro" keys={getFloorKeys(keys, 1)} />
-				<Floor name="Drugie piętro" keys={getFloorKeys(keys, 2)} />
-				<Floor name="Trzecie piętro" keys={getFloorKeys(keys, 3)} />
+		<section className="relative bg-slate-50 rounded-xl overflow-hidden h-full">
+			<div
+				style={{
+					backgroundPosition: "10px 10px",
+				}}
+				className="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
+			<div className="relative rounded-xl overflow-auto">
+				<div className="flex flex-col">
+					<Floor name="Piwnica / Parter" keys={getFloorKeys(keys, 0)} />
+					<Floor name="Pierwsze piętro" keys={getFloorKeys(keys, 1)} />
+					<Floor name="Drugie piętro" keys={getFloorKeys(keys, 2)} />
+					<Floor name="Trzecie piętro" keys={getFloorKeys(keys, 3)} />
+				</div>
 			</div>
 		</section>
 	);
