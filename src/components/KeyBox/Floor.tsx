bf233c24085120defb3cada12037a10566a4ef16
marchewazz <mateuszooospot@gmail.com>
2023-10-10 12:14:35 +0200

    Key and floor styling

--- src/components/KeyBox/Floor.tsx
+++ src/components/KeyBox/Floor.tsx
@@ -9,9 +9,11 @@ export type Props = {
 
 export const Floor = ({ name, keys }: Props) => {
 	return (
-		<div>
-			<div>{name}</div>
-			<div>
+		<div className="bg-purple-600 rounded-xl p-5 border-2 border-white">
+			<div className="text-center text-2xl text-white font-bold italic mb-5">
+				{name}
+			</div>
+			<div className="grid gap-3 sm:gap-5 lg:gap-8 xl:gap-12 grid-cols-4 sm:grid-cols-6 lg:grid-cols-8">
 				{keys.map((key) => (
 					<Key key={key.name} {...key} />
 				))}
