4db338c16594a85578dd6f638b683a67831d3f29
LordPrinz <plazinski2902@gmail.com>
2023-10-11 19:30:27 +0200

    Change grid color

--- src/components/KeyBox/Modal/FreeView.tsx
+++ src/components/KeyBox/Modal/FreeView.tsx
@@ -1,23 +1,27 @@
 import useTeachers from "../../../hooks/useTeachers";
 import { Teacher } from "../../../types/models";
 import CustomSelect from "../../UI/CustomSelect";
-import * as Select from '@radix-ui/react-select';
+import * as Select from "@radix-ui/react-select";
 
 const FreeView = () => {
-
-	const { teachers } = useTeachers()
+	const { teachers } = useTeachers();
 
 	const items = teachers.map((teacher: Teacher) => {
 		return (
-			<Select.Item className="hover:bg-gray-200 cursor-pointer py-2 px-4" value={teacher.id.toString()} key={teacher.id}>
-      			<Select.ItemText>{teacher.name}</Select.ItemText>
-    		</Select.Item>
-		) 
-	})
+			<Select.Item
+				className="hover:bg-gray-200 cursor-pointer py-2 px-4"
+				value={teacher.id.toString()}
+				key={teacher.id}>
+				<Select.ItemText>{teacher.name}</Select.ItemText>
+			</Select.Item>
+		);
+	});
 
-	return <div>
-		<CustomSelect items={items} />
-	</div>;
+	return (
+		<div>
+			<CustomSelect items={items} />
+		</div>
+	);
 };
 
 export default FreeView;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
 			},
 			backgroundImage: {
 				"grid-slate-100": `url(
-					"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23f1f5f9'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e"
+					"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23F1F2F2'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e"
 				)`,
 			},
 			keyframes: {
