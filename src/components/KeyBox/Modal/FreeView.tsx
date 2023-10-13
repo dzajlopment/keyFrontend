1624e07c944591c13c3e8ea9301426da0dae4e19
LordPrinz <plazinski2902@gmail.com>
2023-10-13 20:07:27 +0200

    Style combobox

--- src/components/KeyBox/Modal/FreeView.tsx
+++ src/components/KeyBox/Modal/FreeView.tsx
@@ -1,40 +1,41 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import useTeachers from "../../../hooks/useTeachers";
 import { Teacher } from "../../../types/models";
-import CustomSelect from "../../UI/CustomCombobox";
-import * as Select from '@radix-ui/react-select';
 import { Command } from "cmdk";
 import CustomCombobox from "../../UI/CustomCombobox";
 
-
 const FreeView = () => {
-
-	const { teachers } = useTeachers()
+	const { teachers } = useTeachers();
 
 	const [value, setValue] = useState<Teacher>({
 		id: 0,
-		name: ""
-	})	
+		name: "",
+	});
 
 	const items = teachers.map((teacher: Teacher) => {
 		return (
 			<Command.Item
-				className={`p-1 ${value.id == teacher.id ? "bg-black text-white" : "bg-white text-black hover:bg-gray-300"}`}
-                key={teacher.id}
-                onSelect={() => {
-                  setValue(teacher.id === value.id ? {id: 0, name: "" } : teacher)
-                }}
-				value={teacher.name}
-            >
-              {teacher.name}
-            </Command.Item>
-		) 
-	})
+				className={`p-1 ${
+					value.id == teacher.id
+						? "bg-slate-800 text-slate-100"
+						: "bg-white text-black hover:bg-gray-300"
+				}`}
+				key={teacher.id}
+				onSelect={() => {
+					setValue(teacher.id === value.id ? { id: 0, name: "" } : teacher);
+				}}
+				value={teacher.name}>
+				{teacher.name}
+			</Command.Item>
+		);
+	});
 
-	return <div>
-		<CustomCombobox items={items} />
-	</div>;
+	return (
+		<div>
+			<CustomCombobox items={items} />
+		</div>
+	);
 };
 
 export default FreeView;
